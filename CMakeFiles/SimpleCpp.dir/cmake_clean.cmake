132d64e7fa14f0d7905786db3ed23e1090c42113
Lisa <Lisa@pottier.eu>
2022-02-23 16:03:03 +0100

    [LEVEL 2] START TP

---
+++ CMakeFiles/SimpleCpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SimpleCpp.dir/src/main.cpp.o"
+  "Simple-Cpp-Setup-main/bin/SimpleCpp"
+  "Simple-Cpp-Setup-main/bin/SimpleCpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SimpleCpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
