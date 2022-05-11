46fe45b679ef62cc65671f78962e542f4fecc357
Lisa <Lisa@pottier.eu>
2022-05-11 16:32:55 +0200

    [main] Update

--- Simple-Cpp-Setup-main/src/main.cpp
+++ Simple-Cpp-Setup-main/src/main.cpp
@@ -2,21 +2,33 @@
 #include <random>
 #include <string>
 #include "./menu.hpp"
-#include "./noughts_and_crosses/noughts_and_crosses.hpp"
+//#include "./noughts_and_crosses/noughts_and_crosses.hpp"
 
 #include <p6/p6.h>
 int main(){    
+
+
     
     try {
         // Create the Context by giving the initial size and name of our window
         auto ctx = p6::Context{{1280, 720, "Noughts & Crosses"}};
         // Define the update function. It will be called repeatedly.
         ctx.update = [&]() {
-            // Clear the objects that were drawn during the previous update
-            ctx.background({0.5f, 0.3f, 0.8f});
-            // Draw a circle
-            ctx.circle(p6::Center{ctx.mouse()}, // Centered on the current position of the mouse
-                       p6::Radius{0.3f});       // With a given radius
+            ctx.update = [&]() {    
+                // Clear the background with a fading effect    
+                ctx.use_stroke = false;    
+                ctx.fill       = {0.2f, 0.1f, 0.3f, 0.1f};    
+                for(int i=0;i<=3;i++)
+                {
+                    for(int j=0;j<=3;j++){
+                        ctx.square(p6::TopLeftCorner{i*(720./3.),j*(720./3.)},
+                                    p6::Radius{(720/6)}); 
+                    }
+                }
+                // Draw something    
+                ctx.fill = {1.f, 0.7f, 0.2f};    
+                ctx.circle(p6::Center{ctx.mouse()}, p6::Radius{0.3f});
+            };
         };
         // Start the program
         ctx.start();
--- Simple-Cpp-Setup-main/src/noughts_and_crosses/noughts_and_crosses.cpp
+++ Simple-Cpp-Setup-main/src/noughts_and_crosses/noughts_and_crosses.cpp
@@ -3,3 +3,8 @@
 /// Draws a game board
 /// size is the number of rows and the number of columns
 /// It uses the current context's fill, stroke and stroke_weight
+
+/*void draw_board(int size, p6::Context& ctx)
+{
+                           
+}*/
